e2afcf81b832508fc1209a8a227866007cd78627
unknown <test@test.com>
2018-12-16 16:05:07 +0900

    1216

--- ex1.js
+++ ex1.js
@@ -26,47 +26,35 @@ function mb_btn(e) {
         $(".fa-bars").removeClass("fa-times");
     }
 }
-<<<<<<< HEAD
-$(document).ready(function(){
-    for(var i=1; i<5; i++){
-        // $('#pos'+i).attr('checked');
-        setInterval(function(){
-        console.log(i);
-        },5000);
-    }
-});
 
-        
     
-=======
-// 모달 로그인
-       // Get the modal
-       var modal = document.getElementById('myModal');
+// // 모달 로그인
+//        // Get the modal
+//        var modal = document.getElementById('myModal');
  
-       // Get the button that opens the modal
-       var btn = document.getElementById("myBtn");
+//        // Get the button that opens the modal
+//        var btn = document.getElementById("myBtn");
 
-       // Get the <span> element that closes the modal
-       var span = document.getElementsByClassName("close")[0];                                          
+//        // Get the <span> element that closes the modal
+//        var span = document.getElementsByClassName("close")[0];                                          
 
-       // When the user clicks on the button, open the modal 
-       btn.onclick = function() {
-           modal.style.display = "block";
-       }
+//        // When the user clicks on the button, open the modal 
+//        btn.onclick = function() {
+//            modal.style.display = "block";
+//        }
 
-       // When the user clicks on <span> (x), close the modal
-       span.onclick = function() {
-           modal.style.display = "none";
-       }
+//        // When the user clicks on <span> (x), close the modal
+//        span.onclick = function() {
+//            modal.style.display = "none";
+//        }
 
-       // When the user clicks anywhere outside of the modal, close it
-       window.onclick = function(event) {
-           if (event.target == modal) {
-               modal.style.display = "none";
-           }
-       }
+//        // When the user clicks anywhere outside of the modal, close it
+//        window.onclick = function(event) {
+//            if (event.target == modal) {
+//                modal.style.display = "none";
+//            }
+//        }
 
->>>>>>> eee71215a714fa48bfa4537ca111560760caefb3
 // $(document).ready(function(){
 //     $('.item-container').mouseover(function(){
 //         $('.nav-dropWrap').css("display","block");
